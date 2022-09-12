df96f684105cae056b2d76b6b5fb0605c45cf79f
IgnacioPerez98 <nachopp98@hotmail.com>
2022-09-12 20:43:30 -0300

    Logica

---
+++ src/Library/Logica.cs
@@ -0,0 +1,20 @@
+namespace Roleplay;
+using System.Collections;
+
+public class Logica
+{
+    public static void Atacar(object Atacante, TipoPersonaje personaje1, object atacado, TipoPersonaje personaje2)
+    {
+        
+        
+    }
+
+
+
+}
+
+public enum TipoPersonaje{
+    Elfo,
+    Mago,
+    Enano
+}
