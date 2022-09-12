aeda07a8bcea23c55a970d6146df7fb212e81d66
Andres-Arancio <aarancio97@gmail.com>
2022-09-12 20:26:45 -0300

    Initial commit de program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
-namespace Program
+namespace Roleplay
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Item espada = new Item("Espada de Hierro Frio", 10, 2, 0);
+            Item armadura = new Item("Armadura de Acero", 0, 10, 0);
+            Item baston = new Item("Baston Magico", 2, 1, 0);
         }
     }
 }
